e53c82daa8fe62ee493a7a7cdaac687fd9345db4
Maksat <gangsta.okulus02@gmail.com>
2023-01-26 20:51:00 +0600

    fix/bug-static/add-DB

--- Project/hello/hello/settings.py
+++ Project/hello/hello/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mj92kz%ck8c8z(cee0ni-=s_dqep0j1@-b$k44r6(a-m&8(kyw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # app
-    'firstapp'
+    'firstapp',
 ]
 
 MIDDLEWARE = [
@@ -119,11 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-MEDIA_URL = '/media/'
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+#
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+#
+# MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
